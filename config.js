6d54b9c4887e1a60761039cba91202c4228f0bbf
bronwar320-cmyk <bronwar320@gmail.com>
2026-08-12 12:49:40 -0300

    config.js

--- config.js
+++ config.js
@@ -2,5 +2,5 @@
 // Nunca coloque a senha dos donos neste arquivo.
 window.NA_REGUA_CONFIG = {
   SUPABASE_URL: "https://zxwgkflooprbydijmodv.supabase.co",
-  SUPABASE_ANON_KEY: "sb_secret_aM7bGbi_su_O1H0L3mz2Ng_a7K64bTx"
+  SUPABASE_ANON_KEY: "sb_publishable_RB5xCBktbaz_aIDYPAy-cw_Q_NUqgbR"
 };
