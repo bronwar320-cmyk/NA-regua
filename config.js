bc7c6711abfd1e826bbce786b2cd5c969111cab8
bronwar320-cmyk <bronwar320@gmail.com>
2026-08-12 12:48:52 -0300

    config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // Preencha com as credenciais do SEU projeto Supabase.
 // Nunca coloque a senha dos donos neste arquivo.
 window.NA_REGUA_CONFIG = {
-  SUPABASE_URL: "COLE_AQUI_A_URL_DO_SUPABASE",
-  SUPABASE_ANON_KEY: "COLE_AQUI_A_CHAVE_ANON_PUBLICA"
+  SUPABASE_URL: "https://zxwgkflooprbydijmodv.supabase.co",
+  SUPABASE_ANON_KEY: "sb_secret_aM7bGbi_su_O1H0L3mz2Ng_a7K64bTx"
 };
